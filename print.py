f731f27dcb3ccedf35619e9d3ac9ba61c48b864a
Shohag <mhshohag.ice@gmail.com>
2020-08-23 23:00:27 +0600

    Initial Commit

---
+++ print.py
@@ -0,0 +1,16 @@
+# Clear the terminal screen
+import os
+os.system('clear')
+
+names = ['Shohag', 'Shafin', 'Nahiyan']
+fav_dict = {
+	'Shohag': 'Blue',
+	'Shafin': 'Green',
+	'Nahiyan': 'Yellow',
+}
+
+commitment = False
+if commitment == False:
+	print(fav_dict['Nahiyan'])
+else:
+	print("There was a commitment")
