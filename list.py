5f93dd82e9448e1ebb634eaf8a2cc0ef47baa1d1
Shohag <mhshohag.ice@gmail.com>
2020-09-11 20:34:55 +0600

    len function

--- list.py
+++ list.py
@@ -2,9 +2,10 @@ import os
 os.system('clear')
 
 #list in python
-nums = [1,2,3,4,5]
-names = ['Shohag', 'Shafin', 'Nahiyan', 'Nafis', 5144, 5039, nums]
+#nums = [1,2,3,4,5]
+names = ['Shohag', 'Shafin', 'Nahiyan', 'Nafis', 5144, 5039]
 names.append('Mahin')
 names.append('Majbin')
 
-print(names[6][4])
+#print(names[6][4])
+print(names[len(names)-1])
